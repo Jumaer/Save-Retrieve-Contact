7b803ac904a10861b716c28b7f88068af81e8b9a
jumaer-ahamed <jumaercse@gmail.com>
2024-01-29 22:05:45 +0600

    fetch contact fragment updated

--- app/src/main/java/com/example/myapplication/fragments/FetchContactFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/FetchContactFragment.kt
@@ -1,8 +1,25 @@
 package com.example.myapplication.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentFetchContactBinding
+import com.example.myapplication.databinding.FragmentSaveContactBinding
 
 
 class FetchContactFragment : Fragment() {
 
+    private lateinit var binding : FragmentFetchContactBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFetchContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
