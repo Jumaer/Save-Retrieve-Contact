052cd1eb9703b78c41f085ac842496f32e71a36c
jumaer-ahamed <jumaercse@gmail.com>
2024-01-30 00:05:23 +0600

    comment added

--- app/src/main/java/com/example/myapplication/utils/ViewImageUtil.kt
+++ app/src/main/java/com/example/myapplication/utils/ViewImageUtil.kt
@@ -5,7 +5,11 @@ import android.graphics.Canvas
 import android.view.View
 import java.io.ByteArrayOutputStream
 
-
+/**
+ * This method can convert bitmap to byte array
+ * @param bitmap is that bitmap that need to be converted
+ * @return [ByteArray] is the result ..
+ */
 private fun bitmapToByteArray(bitmap: Bitmap?): ByteArray? {
     if(bitmap == null) return null
 
@@ -14,7 +18,13 @@ private fun bitmapToByteArray(bitmap: Bitmap?): ByteArray? {
     return stream.toByteArray()
 }
 
-
+/**
+ * This method will help to get byte array directly from image view
+ * Because image can be uri, url, file , bitmap ,
+ * so if any thing is present to view then image will be load and send
+ * @param view is any [View] ... here shapable image view will perform better
+ * @return [ByteArray]
+ */
 fun getByteArray(view: View): ByteArray?{
     val bitmap =
         Bitmap.createBitmap(view.layoutParams.width, view.layoutParams.height, Bitmap.Config.ARGB_8888)
