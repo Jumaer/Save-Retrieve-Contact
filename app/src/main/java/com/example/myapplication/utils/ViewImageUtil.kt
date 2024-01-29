717bb48b7fe53745a4f8761724db867dffffde93
jumaer-ahamed <jumaercse@gmail.com>
2024-01-29 23:57:42 +0600

    contact image save updated

--- app/src/main/java/com/example/myapplication/utils/ViewImageUtil.kt
+++ app/src/main/java/com/example/myapplication/utils/ViewImageUtil.kt
@@ -15,16 +15,9 @@ private fun bitmapToByteArray(bitmap: Bitmap?): ByteArray? {
 }
 
 
-fun getByteArray(view: View?): ByteArray? {
-    if(view == null) return null
-    val specSize = View.MeasureSpec.makeMeasureSpec(
-        0 /* any */, View.MeasureSpec.UNSPECIFIED
-    )
-    view.measure(specSize, specSize)
-    val bitmap = Bitmap.createBitmap(
-        view.measuredWidth,
-        view.measuredHeight, Bitmap.Config.ARGB_8888
-    )
+fun getByteArray(view: View): ByteArray?{
+    val bitmap =
+        Bitmap.createBitmap(view.layoutParams.width, view.layoutParams.height, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(bitmap)
     view.layout(view.left, view.top, view.right, view.bottom)
     view.draw(canvas)
